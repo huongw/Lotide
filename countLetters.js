fe251c6d1ac019d5a1dda1db5f2c448ceb6541b7
huongw <whitneyhuong@gmail.com>
2021-08-20 03:08:02 +0000

    completed countLetters function and used test codes

---
+++ countLetters.js
@@ -0,0 +1,46 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(arr1, arr2) {
+
+  const isTrue = eqArrays(arr1, arr2);
+
+  if (isTrue) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const countLetters = function(str) {
+  const obj = {};
+    
+  for (const letter of str) {
+    if (letter in obj) {
+      // letter is in the object already
+      obj[letter] += 1
+    } else {
+      if (letter !== " ") {
+        obj[letter] = 1
+      }
+    }
+    
+  }
+
+  return obj
+}
+
+// let result = countLetters("LHL")
+let result = countLetters("lighthouse labs")
+console.log(result)
+
+assertArraysEqual(result, { l: 2, i: 1, g: 1, h: 2, t: 1, o: 1, u: 1, s: 2, e: 1, a: 1, b: 1 })
