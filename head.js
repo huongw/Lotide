3f04b6515223321519c593f84d57be34c23f241d
huongw <whitneyhuong@gmail.com>
2021-08-23 16:54:11 +0000

    refactored head.js by keeping assertEqual function in a separate file and the test codes in its own separate files.

--- head.js
+++ head.js
@@ -1,22 +1,8 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-// const head = function(arr){
-//   const firstItem = arr[0]
-//   return firstItem
-// }
+const assertEqual = require("./assertEqual");
 
 const head = function(arr){
   return arr[0]
 }
 
-// TEST CODE
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
