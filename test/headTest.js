2cb0527ef7216b37d950ccf93b72f992e1cd2425
huongw <whitneyhuong@gmail.com>
2021-08-23 18:56:19 +0000

    converted headTest.js to use the mocha and chai to assert the tests instead of the original assertEqual

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,18 @@
-const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-// TEST CODE
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require("chai").assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns undefined", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+  it("returns 'Hello'", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
